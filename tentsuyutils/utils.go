31094d1564dbcf5c1d5b91ad6e7c56a0677cf4bd
atolVerderben <atolVerderben@gmail.com>
2020-05-06 12:42:57 -0500

    Add useful rounding function to util package

--- tentsuyutils/utils.go
+++ tentsuyutils/utils.go
@@ -79,3 +79,29 @@ func WhatQuadrant(angle float64) int {
 	}
 	return -1
 }
+
+//RoundFloat takes val and returns a float64 with decimal precision of places
+func RoundFloat(val float64, places int) float64 {
+	roundOn := 0.5
+
+	pow := math.Pow(10, float64(places))
+	digit := pow * val
+	_, div := math.Modf(digit)
+
+	var round float64
+	if val > 0 {
+		if div >= roundOn {
+			round = math.Ceil(digit)
+		} else {
+			round = math.Floor(digit)
+		}
+	} else {
+		if div >= roundOn {
+			round = math.Floor(digit)
+		} else {
+			round = math.Ceil(digit)
+		}
+	}
+
+	return round / pow
+}
